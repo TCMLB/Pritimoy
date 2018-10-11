33e1ae865299de99506fd07d1b0796cb0011e660
pritimoy <pritimoy54@gmail.com>
2018-10-11 22:03:33 +0600

    String print

---
+++ JavaBasic/src/print/PrintString.java
@@ -0,0 +1,21 @@
+//PrintString.java
+//This is simple string print 
+
+package print;
+
+public class PrintString {
+
+	//main class start 
+	public static void main(String[] args) {
+		System.out.print("Hi, This is first java program");//just print
+		System.out.print("My name is pritimoy");
+		System.out.print("\n id: 201500000001");//first print new line then print 
+		System.out.println("My mane is Pritimoy");//print a string with new line
+		System.out.println("hi");
+		System.out.print("my name \n is \n Pritimoy\n");//first print my name then print new line then print
+		                                                 //is then newline then pritimoy
+		System.out.println("Welcome to my repository");
+
+	}//main class end
+
+}// PrintString end
---
+++ README.md
@@ -0,0 +1,1 @@
+# Java program step by step
