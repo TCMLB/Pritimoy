917e8cc9485f55fa9d2cfdc0745d34bdf39b00e4
pritimoy <pritimoy54@gmail.com>
2018-10-31 14:16:59 +0600

    bread and comtinue

---
+++ JavaBasic/src/breakcontinue/ContinueDemo.java
@@ -0,0 +1,16 @@
+package breakcontinue;
+
+public class ContinueDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=100;i=i+3) {
+			if(i ==10) {
+				continue;
+			}
+			System.out.println(i);
+		}//end for
+
+	}
+
+}
